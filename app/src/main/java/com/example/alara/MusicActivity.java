e28424028aa5a1cc0d77bb25842f1b057b91458e
Diego <dredson71@gmail.com>
2019-10-24 12:16:35 -0500

    Musica Play added

--- app/src/main/java/com/example/alara/MusicActivity.java
+++ app/src/main/java/com/example/alara/MusicActivity.java
@@ -2,7 +2,10 @@ package com.example.alara;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -10,14 +13,17 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 
 public class MusicActivity extends AppCompatActivity {
+    boolean val=false;
     private ImageView fotoImagen;
     private TextView txtNombre;
+    private Button botonPla;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
         fotoImagen=(ImageView)findViewById(R.id.fotoPlayCancion);
         txtNombre=(TextView)findViewById(R.id.playCancion);
+        botonPla=(Button)findViewById(R.id.btnPlay);
         Bundle datos= getIntent().getExtras();
         String url= datos.getString("urlcancion");
         String named= datos.getString("nameCancion");
@@ -25,5 +31,22 @@ public class MusicActivity extends AppCompatActivity {
                 .load(url)
                 .into(fotoImagen);
         txtNombre.setText(named);
+        final MediaPlayer mp= MediaPlayer.create(MusicActivity.this,R.raw.scartissue);
+        botonPla.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mp.isPlaying()){
+                    mp.pause();
+                    botonPla.setBackgroundResource(R.drawable.play);
+
+                }else{
+                    mp.start();
+                    botonPla.setBackgroundResource(R.drawable.pause);
+                }
+            }
+        });
+
     }
+
+
 }
